07fcae71026a25c6085242a9af4f022efa01a1bd
Ezio Lemes <eziolemes@gmail.com>
2020-10-20 13:29:33 -0400

    Projeto base configurado

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    singleQuote: true, // aspas simples
+    trailingComma: 'all', // vírgula para separar parametros
+    arrowParens: 'avoid', // não colocar parentes na arrowfunction quando só tiver um parâmetro
+};
